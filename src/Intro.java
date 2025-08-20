8c917af5e253a97e378ff34eb68ff538db13df12
David Lin <davidlin@Davids-MacBook-Air.local>
2025-08-20 10:28:13 -0700

    Day 1

---
+++ src/Intro.java
@@ -0,0 +1,19 @@
+//this is a code comment
+//-comments are used as notes
+//that don't run as code
+
+/*
+this is a block comment
+if you have lots of things to write
+and don't want to use //
+
+in java, classes are program files
+-class names need to match the file name
+-class names should start with a capital letter
+and have no spaces
+-all code we write will go in the curly braces { }
+of the class
+ */
+
+public class Intro {
+}
