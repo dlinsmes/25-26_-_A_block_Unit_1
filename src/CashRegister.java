11cf5b5640a125758326f76287178ecf2525f7e4
davidlin <dlin@smes.org>
2025-08-29 11:38:53 -0700

    day 6

---
+++ src/CashRegister.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class CashRegister {
+
+    public static void main(String [] args) {
+        //user inputs a dollar and cent amount
+        //outputs the minimum quantity of bills and coins to add up to that
+        //amount
+        //denominations:
+        //bills - 100, 20, 10, 5, 1
+        //coins - quarters, dimes, nickels, pennies
+
+        //-use only concepts we've covered so far
+        //only division and modulo for math operators
+        //(no multiplication or subtraction)
+        Scanner input = new Scanner(System.in);
+        System.out.println("how much money do you want?");
+        double amt = input.nextDouble();
+        input.close();
+
+        //start with the largest denomination first
+        //the quantity of bills should be an int bc you can't have
+        //a portion of a bill - take advantage of int div and truncation
+        int hundreds = (int) amt / 100;
+        System.out.println(hundreds + " $100 bills");
+        //find the remaining amt after 100s have been accounted for
+        amt = amt % 100;
+
+
+    }
+}
